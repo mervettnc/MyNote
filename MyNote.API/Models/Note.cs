a78c50980c4d5cf4b41fa9056bfa4c9a3de44a40
Merve Tütüncü <mrvttnc96@gmail.com>
2020-05-20 19:43:06 +0300

    cors & ui references & models

---
+++ MyNote.API/Models/Note.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyNote.API.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime? CreationTime { get; set; }
+
+        [Required]
+        public DateTime? ModificationTime { get; set; }
+
+       
+        public virtual ApplicationUser Author { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+    }
+}

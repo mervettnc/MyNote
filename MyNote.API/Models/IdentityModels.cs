38b52f679024d5a9d7bbfdf7f100d5893f664518
Merve Tütüncü <mrvttnc96@gmail.com>
2020-05-20 22:02:52 +0300

    seed

--- MyNote.API/Models/IdentityModels.cs
+++ MyNote.API/Models/IdentityModels.cs
@@ -28,8 +28,10 @@ namespace MyNote.API.Models
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Configuration.ProxyCreationEnabled = false;
+
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
